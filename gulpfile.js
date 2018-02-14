4e17ecda132a249e79cb7fffa1472ca6844b2858
Stefan Mattern <stefan.v.mattern@web.de>
2018-02-14 14:43:16 -0600

    Network state separation: rocky road

--- gulpfile.js
+++ gulpfile.js
@@ -131,6 +131,8 @@ const sources = {
         './src/main/generic/network/address/PeerId.js',
         './src/main/generic/network/address/PeerAddress.js',
         './src/main/generic/network/address/PeerAddresses.js',
+        './src/main/generic/network/connection/ConnectionPool.js',
+        './src/main/generic/network/connection/PeerConnection.js',
         './src/main/generic/network/message/Message.js',
         './src/main/generic/network/message/AddrMessage.js',
         './src/main/generic/network/message/BlockMessage.js',
